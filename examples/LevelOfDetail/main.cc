79a0df7bf9b3020970c9d58767ccca633eee4337
Jan Khardix Staněk <khardix@gmail.com>
2018-02-26 11:16:19 +0100

    Explicitly request OpenGL version for LOD example

--- examples/LevelOfDetail/main.cc
+++ examples/LevelOfDetail/main.cc
@@ -3,10 +3,19 @@
 
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QSurfaceFormat>
 
 #include "gl_view.hh"
 
 int main(int argc, char *argv[]) try {
+    // Explicitly request OpenGL version 4.5
+    // Otherwise, QtQuick defaults to version 2.1 (!)
+    auto format_request = QSurfaceFormat();
+    format_request.setProfile(QSurfaceFormat::CoreProfile);
+    format_request.setMajorVersion(4);
+    format_request.setMinorVersion(5);
+    QSurfaceFormat::setDefaultFormat(format_request);
+
     const auto &ui_description = QUrl("qrc:/ui.qml");
 
     const auto &app = QGuiApplication(argc, argv);
