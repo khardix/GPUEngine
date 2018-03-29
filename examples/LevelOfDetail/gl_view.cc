a8a4c5a0d993d4199d636fffdea5c059cf5318db
Bc. Jan Staněk <xstane32@stud.fit.vutbr.cz>
2018-03-29 09:26:26 +0200

    Fix cube normal orientation

--- examples/LevelOfDetail/gl_view.cc
+++ examples/LevelOfDetail/gl_view.cc
@@ -167,8 +167,8 @@ std::shared_ptr<ge::gl::VertexArray> GLView::Renderer::load_model()
         5, 6, 7,
         4, 0, 2,
         4, 2, 6,
-        0, 1, 5,
-        0, 5, 4,
+        0, 5, 1,
+        0, 4, 5,
         2, 3, 7,
         2, 7, 6,
     };
