37a547388e6fd57ddcff9dfd7ebba86e4e021d24
Bc. Jan Staněk <xstane32@stud.fit.vutbr.cz>
2018-05-05 20:13:56 +0200

    Clean up half-edge collapse implementation

--- geAd/LevelOfDetail/src/operator/edge_collapse.cpp
+++ geAd/LevelOfDetail/src/operator/edge_collapse.cpp
@@ -18,6 +18,32 @@ namespace lod {
 namespace oper {
 using operation::HalfEdgeTag;
 
+/** Connects neighbours of two edges to each other.
+ * Combined with correct edge adjustments,
+ * this has the effect of removing a triangle from the mesh.
+ * @param[in] lhs One of the edges to detach.
+ * @param[in] rhs The other edge to detach.
+ * @returns Pointer to one of the neighbours.
+ * If one of the input edges was on boundary,
+ * returns the neighbour of the other one (== valid neighbour).
+ * If both edges were boundary, returns nullptr.
+ */
+graph::DirectedEdge *connect_neighbours(
+    graph::DirectedEdge *const lhs, graph::DirectedEdge *const rhs)
+{
+    auto lneigh = nonstd::get<graph::DirectedEdge *>(lhs->neighbour);
+    auto rneigh = nonstd::get<graph::DirectedEdge *>(rhs->neighbour);
+
+    if (lneigh != nullptr) {
+        lneigh->neighbour = rneigh;
+    }
+    if (rneigh != nullptr) {
+        rneigh->neighbour = lneigh;
+    }
+
+    return lneigh == nullptr ? rneigh : lneigh;
+}
+
 /** Applies the operator to the mesh.
  * @param mesh The mesh to be modified.
  * @param operation The operation to perform.
@@ -35,25 +61,16 @@ auto EdgeCollapse<HalfEdgeTag>::operator()(
     auto to_delete = Mesh::EdgeSet{};
 
     const auto edge = operation.element().get();
-    const auto target = edge->target;
-    const auto origin = edge->previous->target;
+    const auto target_node = edge->target;
+    const auto origin_node = edge->previous->target;
 
-    auto edge_ring = opposite_edges(*origin);
+    auto       edge_ring = opposite_edges(*origin_node);
+    const auto edge_ring_complete
+        = edge_ring.front()->previous->target == edge_ring.back()->target;
 
-    auto connect_neighbours = [](const auto &lhs, const auto &rhs) {
-        auto lneigh = nonstd::get<DirectedEdge *>(lhs->neighbour);
-        auto rneigh = nonstd::get<DirectedEdge *>(rhs->neighbour);
-
-        if (lneigh != nullptr) {
-            lneigh->neighbour = rneigh;
-        }
-        if (rneigh != nullptr) {
-            rneigh->neighbour = lneigh;
-        }
-
-        return lneigh == nullptr ? rneigh : lneigh;
-    };
-
+    /* Marks all edges from a triangle for deletion,
+     * and remove them from nodes that refer to them.
+     */
     auto mark_triangle_deleted = [&mesh_edges, &to_delete](auto &&start_edge) {
         for (auto &&edge : start_edge->triangle_edges()) {
             if (edge->previous->target->edge == edge) {
@@ -70,40 +87,51 @@ auto EdgeCollapse<HalfEdgeTag>::operator()(
 
     // Apply the edge adjustments
     for (auto &&opposite : edge_ring) {
-        if (opposite->target == target) {  // replaced by previous triangle
+        // replaced by previous triangle
+        if (opposite->target == target_node) {
             mark_triangle_deleted(opposite);
+            // replace deleted edge with valid neighbour
             opposite = connect_neighbours(opposite->previous, opposite);
             continue;
         }
 
-        if (opposite->previous->target == target) {
-            // replaced by next triangle
+        // replaced by next triangle
+        if (opposite->previous->target == target_node) {
             mark_triangle_deleted(opposite);
+            // replace deleted edge with valid neighbour
             opposite = connect_neighbours(opposite->next(), opposite);
             continue;
         }
 
         // connect outgoing edge to new target
         auto next_edge = opposite->next();
-        next_edge->target = target;
+        next_edge->target = target_node;
 
-        // mark both edges as modified
-        modified.insert({next_edge, opposite->previous});
+        // mark all edges as modified
+        modified.insert({next_edge, opposite, opposite->previous});
     }
 
     // fix nodes with deleted references
     for (const auto &opposite : edge_ring) {
         if (opposite == nullptr) {
-            throw std::runtime_error("Cannot collapse last two triangles!");
+            throw std::runtime_error("Non-manifold collapse!");
         }
 
-        if (opposite->target->edge == nullptr) {
-            opposite->target->edge = opposite->next();
+        auto node = opposite->target;
+        if (node->edge == nullptr) {
+            node->edge = opposite->next();
+        }
+    }
+
+    if (!edge_ring_complete) {
+        auto &first_node = edge_ring.front()->previous->target;
+        if (first_node->edge == nullptr) {
+            first_node->edge = edge_ring.front();
         }
     }
 
     // remove collapsed node and deleted edges
-    mesh.nodes().erase(*origin);
+    mesh.nodes().erase(*origin_node);
     for (auto &&deleted : to_delete) {
         mesh.edges().erase(deleted);
     }
